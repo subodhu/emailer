f9b5bf960f16a99277fedb3198bf2e5911849ecf
Subodh <subodh.ur@gmail.com>
2023-03-29 22:03:56 +0545

    email/email-template api with filter, swagger doc and tracking view

--- apps/emails/api/v1/views.py
+++ apps/emails/api/v1/views.py
@@ -1,6 +1,5 @@
 from django_filters import rest_framework as filters
-from rest_framework.mixins import (CreateModelMixin, ListModelMixin,
-                                   RetrieveModelMixin)
+from rest_framework.mixins import CreateModelMixin, ListModelMixin, RetrieveModelMixin
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.viewsets import GenericViewSet, ModelViewSet
 
